860216270f6f4fa6eef6353d2e0c223cc9fdaeed
Raeshelle Rose <raeshellerose@outlook.com>
2022-07-05 11:36:48 -0500

    update syntax

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+document.getElementById('getButton').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const alienName = document.querySelector('input').value;
+    const alienName = document.querySelector('input').value
     try{
-        const res = await  fetch(`https://star-trek-aliens-feild-guide.herokuapp.com/api/${alienName}`);
-        const data = await res.json()
+        const response = await fetch(`https://star-trek-aliens-feild-guide.herokuapp.com/api/${alienName}`)
+        const data = await response.json()
+
         console.log(data)
-    }
-    catch(error){
+        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById('alienWorld').innerText = data.homeworld
+        document.getElementById('alienFeatures').innerText = data.features
+        document.getElementById('alienFacts').innerText = data.interestingFact
+        document.getElementById('alienExamples').innerText = data.notableExamples
+        document.getElementById('alienImage').src = data.image
+        document.getElementById('alienCaption').innerText = data.speciesName
+    }catch(error){
         console.log(error)
     }
-};
+}
